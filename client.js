f7c73834921945894acb419b8d5b80064f4a132a
neethu-ms <msneethu2989@gmail.com>
2020-02-12 18:02:01 +0000

    Added code to send name to serverA

--- client.js
+++ client.js
@@ -10,6 +10,10 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+  conn.on('connect',()=>{
+    console.log("Successfully connected to game server");
+    conn.write('Name: MSN');
+  });
   conn.on('data',(data) => {
     console.log('Server Says: ', data);
   });
