5fac956d72a23f152f6d50cd2b7457f629e7cd39
neethu-ms <msneethu2989@gmail.com>
2020-02-12 15:00:12 +0000

    Established connection with SErver

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+console.log('Connecting ...');
+
+connect().on('data',(data) => {
+  console.log('Server Says: ', data);
+});
+
+connect();
