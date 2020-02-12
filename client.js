7b7860afaa6ed27971df1c5a2f10c057769cb82d
neethu-ms <msneethu2989@gmail.com>
2020-02-12 18:32:41 +0000

    Experimented movements

--- client.js
+++ client.js
@@ -13,12 +13,23 @@ const connect = function() {
   conn.on('connect',()=>{
     console.log("Successfully connected to game server");
     conn.write('Name: MSN');
-  });
-  conn.on('data',(data) => {
-    console.log('Server Says: ', data);
-  });
-  return conn;
-};
+    
+   /*  setInterval(() => {
+      conn.write('Move: up');
+    },100);
 
+    setInterval(() => {
+      conn.write('Move: right');
+    },200); */
+
+    
+    
+    conn.on('data',(data) => {
+      console.log('Server Says: ', data);
+    });
+
+    return conn;
+  });
+};
 module.exports = { connect
 };
